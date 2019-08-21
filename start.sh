ecb2bd2d15f56c4de1dae4c86bdd601d4b094580
Spencer Hughes <16389711+spencerhughes@users.noreply.github.com>
2019-08-21 11:46:57 -0400

    Allows the user to set the admin password and stream key

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-usermod -u ${PUID} ${CONT_USER}
-groupmod -g ${PGID} ${CONT_USER}
+usermod -u ${PUID} ${CONT_USER} > /dev/null 2>&1
+groupmod -g ${PGID} ${CONT_USER} > /dev/null 2>&1
+sed -i -e "s/\"AdminPassword\": \"DBA5E73E4EA26E377273\",/\"AdminPassword\": \"$ADMIN_PASSWORD\",/g" -e "s/\"StreamKey\": \"ALongStreamKey\",/\"StreamKey\": \"$STREAM_KEY\",/g" settings.json
 su -s /bin/bash -c "${CONT_CMD}" ${CONT_USER}
